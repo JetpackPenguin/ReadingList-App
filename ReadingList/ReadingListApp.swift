fe6e5e6ce376c15908d29bc7bfd430d69b36afc8
Jon <jonathan.russo1216@gmaill.com>
2026-05-16 02:22:01 -0400

    Initial Commit

---
+++ ReadingList/ReadingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingListApp.swift
+//  ReadingList
+//
+//  Created by Jon on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ReadingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
